7d136b13313c4fdb66f95b4d8e2f3b4bbb74ea28
hurley25 <liuhuan1992@gmail.com>
2014-04-14 13:47:07 +0800

    增加cmake示例

---
+++ cmake/test2/src/main.c
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014年04月14日 12时08分31秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hurley (LiuHuan), liuhuan1992@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+
+	return EXIT_SUCCESS;
+}
+
